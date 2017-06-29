42ce97c1415c3d8f068bb6b5a78cc1dd13da58a0
Naveen Kumar Kanumoori <naveenkanumoori@gmail.com>
2017-06-29 17:32:24 -0400

    HeaderComponent, CarsCamponent, CarDetailsComponent, Car Model, CarService added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CarsComponent } from './components/cars/cars.component';
+import {RouterModule,Routes} from "@angular/router";
+import { CardetailsComponent } from './components/cardetails/cardetails.component';
+import {CarsService} from "./providers/cars.service";
+import {HttpModule} from "@angular/http";
+
+const appRoutes: Routes = [
+  { path: 'cars', component: CarsComponent },
+  { path: 'cars/:id', component: CardetailsComponent },
+  { path: '', redirectTo: '/cars', pathMatch: 'full' },
+  { path: '**', component: CarsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarsComponent,
+    CardetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpModule
   ],
-  providers: [],
+  providers: [CarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
